90ed3a7cec0e64215e85b86044fba498539bf00a
PixelParty <hello@pixelparty.ru>
2016-01-06 20:32:18 +0300

    add relation between Finance and deliveries

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -37,59 +37,80 @@ class FinanceController extends Controller
 			$stek[$i][4] = $delivery->id;
 			$i++;
        	}
-       	usort($stek,function($a, $b){
-       		return $a[2] - $b[2];
-       	});
-
 
        	$size = count($stek);
+       	if ($size > 1){
+	       	usort($stek,function($a, $b){
+	       		return $a[2] - $b[2];
+	       	});
+	    }
+
+	    $keyStek = [];
        	$client = $stek[0][0];
    		$sum = (float)$stek[0][1];
    		$number_of_waybill = 1;
    		$registry = $stek[0][2];
    		$date_of_registry = $stek[0][3];
-   		$deliveryId = $stek[0][4]; 
+   		array_push($keyStek,$stek[0][4]);
+   		if ($size > 1){
+	       	for ($i=1; $i<$size; $i++){
+	       		if($registry === $stek[$i][2]){
+		       		$sum = $sum +(float)$stek[$i][1];
+		       		$number_of_waybill++; 
+		       		array_push($keyStek,$stek[$i][4]);
 
-       	for ($i=1; $i<$size; $i++){
-       		if($registry === $stek[$i][2]){
-	       		$sum = $sum +(float)$stek[$i][1];
-	       		$number_of_waybill++;
-	       		$deliveryId = $deliveryId + '$' +$stek[$i][4]; 
+		       		if ($i === $size - 1){
+		       			$this->saveFinance($client,$sum,$number_of_waybill,$registry,$date_of_registry,$keyStek);
+		       		}
+	       		}else{
+		       		$this->saveFinance($client,$sum,$number_of_waybill,$registry,$date_of_registry,$keyStek);
 
-	       		if ($i === $size - 1){
-	       			$finance = new Finance;
-		       		$finance->client = $client;
-		       		$finance->sum = $sum;
-		       		$finance->number_of_waybill = $number_of_waybill;
-		       		$finance->type_of_funding = "Первый платеж";
-		       		//$finance->date_of_funding
-		       		$finance->registry = $registry;
-		       		$finance->date_of_registry = $date_of_registry;
-		       		$finance->status = "К финансированию";
-		       		$finance->deliveryId = $deliveryId;
-		       		$finance->save();
-	       		}
-       		}else{
-	       		$finance = new Finance;
-	       		$finance->client = $client;
-	       		$finance->sum = $sum;
-	       		$finance->number_of_waybill = $number_of_waybill;
-	       		$finance->type_of_funding = "Первый платеж";
-	       		//$finance->date_of_funding
-	       		$finance->registry = $registry;
-	       		$finance->date_of_registry = $date_of_registry;
-	       		$finance->status = "К финансированию";
-	       		$finance->deliveryId = $deliveryId;
-	       		$finance->save();
+		       		$size = count($stek);
+			       	$client = $stek[$i][0];
+			   		$sum = (float)$stek[$i][1];
+			   		$number_of_waybill = 1;
+			   		$registry = $stek[$i][2];
+			   		$date_of_registry = $stek[$i][3];
+			   		array_push($keyStek,$stek[$i][4]);
 
-	       		$size = count($stek);
-		       	$client = $stek[$i][0];
-		   		$sum = (float)$stek[$i][1];
-		   		$number_of_waybill = 1;
-		   		$registry = $stek[$i][2];
-		   		$date_of_registry = $stek[$i][3];
-		   		$deliveryId = $stek[$i][4]; 
-       		}
+			   		if ($i === $size - 1){
+		       			$this->saveFinance($client,$sum,$number_of_waybill,$registry,$date_of_registry,$keyStek);
+		       		}
+		       	}	    
+		    }
+	    }else{
+	    	$this->saveFinance($client,$sum,$number_of_waybill,$registry,$date_of_registry,$keyStek);
        	}
+	}
+
+    protected function saveFinance($client,$sum,$number_of_waybill,$registry,$date_of_registry,$keyStek){
+    	$finance = new Finance;
+   		$finance->client = $client;
+   		$finance->sum = $sum;
+   		$finance->number_of_waybill = $number_of_waybill;
+   		$finance->type_of_funding = "Первый платеж";
+   		//$finance->date_of_funding
+   		$finance->registry = $registry;
+   		$finance->date_of_registry = $date_of_registry;
+   		$finance->status = "К финансированию";
+   		if ($finance->save()){
+   			$this->saveKey($keyStek);
+   			$keyStek = [];
+   		};
+    }
+
+    protected function saveKey($keyStek){
+    	if (Finance::max('id')){
+   			$financeMaxId = Finance::max('id');
+   		}else{
+   			$financeMaxId = 1;
+   		}
+   		
+   		foreach ($keyStek as $key){
+   			$deliveryFinanceId = Delivery::find($key);
+   			$deliveryFinanceId->finance_id = $financeMaxId;
+   			$deliveryFinanceId->save();
+   		}
     }
 }
+//
